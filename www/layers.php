bc44c361233d188025a7864a7efa5ede99f05732
Anrijs <>
2019-04-23 07:02:49 +0300

    layers: added layer url template

--- www/layers.php
+++ www/layers.php
@@ -13,14 +13,16 @@
     $zoom = file_get_contents($_R["layers"] . $layer . "/zoom");
     $maps = explode(";",file_get_contents($_R["layers"] . $layer . "/maps"));
 
-    $body .= '<tr><td>'.$no++.'</td><td>'.$name.'</td><td>'.$zoom.'</td><td>'.(sizeof($maps)-1).'</td><td><a href="map.php#layers='.$name.'" class="btn btn-sm btn-info" style="margin-right:4px;">View layer</a><a href="#" class="btn btn-sm btn-danger">Delete</a></td>';
+    $namehtml = $name . '<br><small>'.$layer.'/{z}/{x}/{y}.png</small>';
+
+    $body .= '<tr><td>'.$no++.'</td><td>'.$namehtml.'</td><td>'.$zoom.'</td><td>'.(sizeof($maps)-1).'</td><td><a href="map.php#layers='.$name.'" class="btn btn-sm btn-info" style="margin-right:4px;">View layer</a><a href="#" class="btn btn-sm btn-danger">Delete</a></td>';
   }
   $body .= '</table>';
   $body .= '<div class="float-right"><a href="layers.add.php" class="btn btn-info">Add layer</a></div>';
 
   $contents["tab"] = "Layers";
-  $contents["header"] = ""; 
-  $contents["script"] = ""; 
+  $contents["header"] = "";
+  $contents["script"] = "";
   $contents["body"] = $body;
   // draw template
   include 'templates/template.main.php';
