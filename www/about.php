0766386c0eaa9fc84f74169836092de960665426
Anrijs <>
2020-02-16 21:22:25 +0200

    about: updated main site

--- www/about.php
+++ www/about.php
@@ -4,7 +4,7 @@
 
   $body  = "<h1> About</h1><hr class=\"alt\">";
 
-  $body .= "<b>GeoTIFF Tiler</b> by <b>Anrijs</b> | <a href=\"https://spaaace.lv\">spaaace.lv</a><br>";
+  $body .= "<b>GeoTIFF Tiler</b> by <b>Anrijs</b> | <a href=\"https://anrijs.lv\">anrijs.lv</a><br>";
   $body .= "Git: <a href=\"https://github.com/Anrijs/GeoTIFF-Tiler\">GeoTIFF Tiler</a>";
 
   $contents["tab"] = "About";
